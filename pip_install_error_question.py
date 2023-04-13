b469c95f7cc8897622855fa849dd0d11b7d883cf
CraftyAubrey <127454368+CraftyAubrey@users.noreply.github.com>
2023-04-12 19:03:31 -0500

    Create pip_install_error_question.py
    Error message I am getting in final lab Hands-on Lab: Analyzing Historical Stock/Revenue Data and Building a Dashboard

---
+++ pip_install_error_question.py
@@ -0,0 +1,15 @@
+#I have both the link and the error message isolated:
+
+#link to my lab: 
+https://labs.cognitiveclass.ai/v2/tools/jupyterlab?ulid=ulid-f94392bb53ad3442b4d0e743795f64b44faf343f
+
+
+
+#Error message in question after trying to load yfinance, etc: 
+
+ERROR: pip's dependency resolver does not currently take into account all the packages that are installed. This behaviour is the source of the following dependency conflicts.
+nbconvert 7.2.6 requires nbformat>=5.1, but you have nbformat 4.2.0 which is incompatible.
+nbclient 0.7.2 requires nbformat>=5.1, but you have nbformat 4.2.0 which is incompatible.
+jupyter-server 1.23.3 requires nbformat>=5.2.0, but you have nbformat 4.2.0 which is incompatible.
+Successfully installed nbformat-4.2.0
+
